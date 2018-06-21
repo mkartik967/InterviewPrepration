3fc31f39f7e9f36ada36e4728f819fce974490c7
mkartik967 <40457859+mkartik967@users.noreply.github.com>
2018-06-21 20:11:50 +0530

    Create CHCOINSG.cpp

---
+++ CHCOINSG.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define mod 1000007
+#define pb push_back
+using namespace std;
+typedef pair<int,int>p;
+ 
+int main(){
+ll	int t,n,i,flag,sub;
+	cin>>t;
+	while(t--){
+		cin>>n;
+	if(n%6==0)
+	cout<<"Misha"<<endl;
+	else
+	cout<<"Chef"<<endl;
+}
+return 0;
+} 
